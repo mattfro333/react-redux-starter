eda340f4fdf6310df878da583778b4328192fbb3
mattfro333 <mattfro333@gmail.com>
2018-11-19 10:08:54 -0700

    adding redux

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -1,6 +1,14 @@
 import React, {PropTypes} from 'react';
 
 class CoursesPage extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+
+    this.state = {
+      course: { titles: null }
+    };
+  }
+
   render() {
     return (
       <div>
@@ -10,6 +18,14 @@ class CoursesPage extends React.Component {
           <p>Readin</p>
           <p>Scribble Scratch</p>
           <p>Kung Foolery</p>
+        <input
+          type="text"
+          onChange={this.onTitleChange}
+          value={this.state.course.title} />
+        <input
+          type="submit"
+          value="Save"
+          onClick={this.onClickSave} />
       </div>
     );
   }
